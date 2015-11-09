0ae0a7f0dc95fdaa3d3853c6a2c3a90808a9ba51
Vivian Keating <vkeat660@gmail.com>
2015-11-09 15:16:40 -0500

    Initial commit

---
+++ app/src/main/java/com/keating/vivian/omgandroid/MainActivity.java
@@ -0,0 +1,26 @@
+package com.keating.vivian.omgandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+
+
+
+
+}
